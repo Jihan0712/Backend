c2235a552bfac5d37bff2af4896a713cfeac3756
legaspi0621 <christianryle.legaspi@gmail.com>
2024-06-05 15:47:28 +0800

    stats

---
+++ controllers/statisticsController.js
@@ -0,0 +1,24 @@
+const SmokeTest = require('../models/SmokeTest')
+const User = require('../models/User')
+
+const getStatistics = async (req, res) => {
+  try {
+    const totalUsers = await User.countDocuments()
+    const totalPassed = await SmokeTest.countDocuments({ smoke_result: 'Passed' })
+    const totalFailed = await SmokeTest.countDocuments({ smoke_result: 'Failed' })
+    const opacityData = await SmokeTest.find({}, 'opacity smoke_result createdAt').lean()
+
+    res.json({
+      totalUsers,
+      totalPassed,
+      totalFailed,
+      opacityData
+    })
+  } catch (error) {
+    res.status(500).json({ message: 'Server error' })
+  }
+}
+
+module.exports = {
+  getStatistics
+}
